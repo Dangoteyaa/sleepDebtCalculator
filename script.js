ca534c10c7fd771e7d5586f1ff778e5cc91cdb90
Stephanie Ferrier <sferrier96@outlook.com>
2025-07-22 22:09:19 +0100

    basic HTML, added a notepad page about how I want the ap to behave, linked CSS and JS pages to HTML

---
+++ script.js
@@ -0,0 +1,7 @@
+// get userInput
+//compare this against 8 hours of sleep which is the ideal number of hours of sleep one should have per night. 
+// the sleep debt is 8 hours - userInput
+//if the sleep debt is high link to resources and gives tips and tricks to help this improve
+// if sleep debt is low then congratulate them
+
+let userInput = prompt ('Please enter how many hours you slept last night')
