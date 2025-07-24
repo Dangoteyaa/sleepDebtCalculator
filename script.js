b4718003bd79b376cfbb5a0465968614f710316f
Stephanie Ferrier <sferrier96@outlook.com>
2025-07-24 22:04:31 +0100

    done!

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const formScreen = document.getElementById("formScreen");
 const resultScreen = document.getElementById("resultScreen");
 const sleepDebtResult = document.getElementById("sleepDebtResult");
 const formInput = document.getElementById("formInput");
-const form = document.getElementById("theForm");
 const button = document.getElementById("calculateButton");
 
 button.addEventListener("click", calculateSleepDebt);
@@ -18,22 +17,36 @@ function calculateSleepDebt(e) {
     const idealHours = 8;
     let userInput = Number(formInput.value); 
     let sleepDebt = idealHours - userInput;
-    if(!userInput){
+   
+    if(!userInput && userInput !== 0){
         alert ('⛔ Please enter the number of hours you slept last night');
-    } else if (sleepDebt === 0) {
-        message = `😴 You overslept by ${sleepDebt} hour(s).`;
-        return;    
-    } else {
+        return;
+    } else if (sleepDebt === 0 ) {
+        message = "Congrats! You've had enough sleep! 🎉";
+    } else if (sleepDebt === 1) {
+        message = "⚠️ You're an hour short on sleep";        
+    }
+    else if (userInput < 8) {
         message = `⚠️ You're ${sleepDebt} hours short on sleep`;
-        return;        
+        
+    } else if (sleepDebt < 0) {
+        message = `😴 You overslept by ${Math.abs(sleepDebt)} hours`;       
     }
 
-        sleepDebtResult = message;
+        sleepDebtResult.textContent = message;
 
         formScreen.classList.remove('active');
         resultScreen.classList.add('active');
 }
 
+//logic for go back button
+
+function goBack(){
+    formScreen.classList.add('active');
+    resultScreen.classList.remove('active');
+    formInput.value = "";
+}
+ 
 
 
 
